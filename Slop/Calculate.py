9db260da236ddda6584501c0b4841131b68a09c0
babyblue <wszwsz179@163.com>
2022-09-29 13:38:58 +0800

    modify function.

--- Slop/Calculate.py
+++ Slop/Calculate.py
@@ -1,167 +1,61 @@
+import math
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from tqdm import trange
+import seaborn as sns
+import matplotlib.colors
 
 
 def cal_gradient_horizon(nums, x, y, x_cellsize):
-    ans = (nums[x - 1][y + 1] + 2 * nums[x - 1][y + 1] + nums[x + 1][y + 1] -
+    ans = (nums[x - 1][y + 1] + 2 * nums[x][y + 1] + nums[x + 1][y + 1] -
            (nums[x - 1][y - 1] + 2 * nums[x][y - 1] + nums[x + 1][y - 1])) \
           / (8 * x_cellsize)
     return ans
 
 
 def cal_gradient_vertical(nums, x, y, y_cellsize):
-    ans = (nums[x - 1][y + 1] + 2 * nums[x - 1][y + 1] + nums[x + 1][y + 1] -
-           (nums[x - 1][y - 1] + 2 * nums[x][y - 1] + nums[x + 1][y - 1])) \
+    ans = (nums[x + 1][y - 1] + 2 * nums[x + 1][y] + nums[x + 1][y + 1] -
+           (nums[x - 1][y - 1] + 2 * nums[x - 1][y] + nums[x - 1][y + 1])) \
           / (8 * y_cellsize)
     return ans
 
 
-def window3x3(arr, shape=(3, 3)):
-    r_win = np.floor(shape[0] / 2).astype(int)
-    c_win = np.floor(shape[1] / 2).astype(int)
-    x, y = arr.shape
-    for i in range(x):
-        xmin = max(0, i - r_win)
-        xmax = min(x, i + r_win + 1)
-        for j in range(y):
-            ymin = max(0, j - c_win)
-            ymax = min(y, j + c_win + 1)
-            yield arr[xmin:xmax, ymin:ymax]
+def cal_single_point_slope(nums, height, width, x, y, x_cellsize, y_cellsize):
+    if x == 0 or x == height - 1 or y == 0 or y == width - 1:
+        return 0.0
+    dzdx = cal_gradient_horizon(nums, x, y, x_cellsize)
+    dzdy = cal_gradient_vertical(nums, x, y, y_cellsize)
+    ans = math.atan(math.sqrt(dzdx ** 2 + dzdy ** 2)) * 57.29578
+    return ans
 
 
-def gradient(XYZ_file, min=0, max=15, figsize=(15, 10), **kwargs):
-    """
-
-    :param XYZ_file: XYZ file in the following format: x,y,z (inlcuding headers)
-    :param min: color bar minimum range.
-    :param max: color bar maximum range.
-    :param figsize: figure size.
-    :param kwargs:
-           plot: to plot a gradient map. Default is True.
-    :return: returns an array with the shape of the grid with the computed slopes
-
-
-    The algorithm calculates the gradient using a first-order forward or backward difference on the corner points, first
-    order central differences at the boarder points, and a 3x3 moving window for every cell with 8 surrounding cells (in
-    the middle of the grid) using a third-order finite difference weighted by reciprocal of squared distance
-
-    Assumed 3x3 window:
-
-                                    -
-                        |   a   |   b   |   c   |
-                                    -
-                        |   d   |   e   |   f   |
-                                    -
-                        |   g   |   h   |   i   |
-                                    -
-
-
-    """
-
-    kwargs.setdefault('plot', True)
-
-    grid = XYZ_file.to_numpy()
-
-    nx = XYZ_file['x'].unique().size
-    ny = XYZ_file['y'].unique().size
-
-    xs = grid[:, 0].reshape(ny, nx, order='F')
-    ys = grid[:, 1].reshape(ny, nx, order='F')
-    zs = grid[:, 2].reshape(ny, nx, order='F')
-    dx = abs((xs[:, 1:] - xs[:, :-1]).mean())
-    dy = abs((ys[1:, :] - ys[:-1, :]).mean())
-
-    gen = window3x3(zs)
-    windows_3x3 = np.asarray(list(gen))
-    windows_3x3 = windows_3x3.reshape(ny, nx)
-
-    dzdx = np.empty((ny, nx))
-    dzdy = np.empty((ny, nx))
-    loc_string = np.empty((ny, nx), dtype="S25")
-
-    for ax_y in trange(ny):
-        for ax_x in range(nx):
-
-            # corner points
-            if ax_x == 0 and ax_y == 0:  # top left corner
-                dzdx[ax_y, ax_x] = (windows_3x3[ax_y, ax_x][0][1] - windows_3x3[ax_y, ax_x][0][0]) / dx
-                dzdy[ax_y, ax_x] = (windows_3x3[ax_y, ax_x][1][0] - windows_3x3[ax_y, ax_x][0][0]) / dy
-                loc_string[ax_y, ax_x] = 'top left corner'
-
-            elif ax_x == nx - 1 and ax_y == 0:  # top right corner
-                dzdx[ax_y, ax_x] = (windows_3x3[ax_y, ax_x][0][1] - windows_3x3[ax_y, ax_x][0][0]) / dx
-                dzdy[ax_y, ax_x] = (windows_3x3[ax_y, ax_x][1][1] - windows_3x3[ax_y, ax_x][0][1]) / dy
-                loc_string[ax_y, ax_x] = 'top right corner'
-
-            elif ax_x == 0 and ax_y == ny - 1:  # bottom left corner
-                dzdx[ax_y, ax_x] = (windows_3x3[ax_y, ax_x][1][1] - windows_3x3[ax_y, ax_x][1][0]) / dx
-                dzdy[ax_y, ax_x] = (windows_3x3[ax_y, ax_x][1][0] - windows_3x3[ax_y, ax_x][0][0]) / dy
-                loc_string[ax_y, ax_x] = 'bottom left corner'
-
-            elif ax_x == nx - 1 and ax_y == ny - 1:  # bottom right corner
-                dzdx[ax_y, ax_x] = (windows_3x3[ax_y, ax_x][1][1] - windows_3x3[ax_y, ax_x][1][0]) / dx
-                dzdy[ax_y, ax_x] = (windows_3x3[ax_y, ax_x][1][1] - windows_3x3[ax_y, ax_x][0][1]) / dy
-                loc_string[ax_y, ax_x] = 'bottom right corner'
-
-            # top boarder
-            elif (ax_y == 0) and (ax_x != 0 and ax_x != nx - 1):
-                dzdx[ax_y, ax_x] = (windows_3x3[ax_y, ax_x][0][-1] - windows_3x3[ax_y, ax_x][0][0]) / (2 * dx)
-                dzdy[ax_y, ax_x] = (windows_3x3[ax_y, ax_x][1][1] - windows_3x3[ax_y, ax_x][0][1]) / dy
-                loc_string[ax_y, ax_x] = 'top boarder'
-
-            # bottom boarder
-            elif ax_y == ny - 1 and (ax_x != 0 and ax_x != nx - 1):
-                dzdx[ax_y, ax_x] = (windows_3x3[ax_y, ax_x][1][-1] - windows_3x3[ax_y, ax_x][1][0]) / (2 * dx)
-                dzdy[ax_y, ax_x] = (windows_3x3[ax_y, ax_x][1][1] - windows_3x3[ax_y, ax_x][0][1]) / dy
-                loc_string[ax_y, ax_x] = 'bottom boarder'
-
-            # left boarder
-            elif ax_x == 0 and (ax_y != 0 and ax_y != ny - 1):
-                dzdx[ax_y, ax_x] = (windows_3x3[ax_y, ax_x][1][1] - windows_3x3[ax_y, ax_x][1][0]) / dx
-                dzdy[ax_y, ax_x] = (windows_3x3[ax_y, ax_x][-1][0] - windows_3x3[ax_y, ax_x][0][0]) / (2 * dy)
-                loc_string[ax_y, ax_x] = 'left boarder'
-
-            # right boarder
-            elif ax_x == nx - 1 and (ax_y != 0 and ax_y != ny - 1):
-                dzdx[ax_y, ax_x] = (windows_3x3[ax_y, ax_x][1][1] - windows_3x3[ax_y, ax_x][1][0]) / dx
-                dzdy[ax_y, ax_x] = (windows_3x3[ax_y, ax_x][-1][-1] - windows_3x3[ax_y, ax_x][0][-1]) / (2 * dy)
-                loc_string[ax_y, ax_x] = 'right boarder'
-
-            # middle grid
-            else:
-                a = windows_3x3[ax_y, ax_x][0][0]
-                b = windows_3x3[ax_y, ax_x][0][1]
-                c = windows_3x3[ax_y, ax_x][0][-1]
-                d = windows_3x3[ax_y, ax_x][1][0]
-                f = windows_3x3[ax_y, ax_x][1][-1]
-                g = windows_3x3[ax_y, ax_x][-1][0]
-                h = windows_3x3[ax_y, ax_x][-1][1]
-                i = windows_3x3[ax_y, ax_x][-1][-1]
-
-                dzdx[ax_y, ax_x] = ((c + 2 * f + i) - (a + 2 * d + g)) / (8 * dx)
-                dzdy[ax_y, ax_x] = ((g + 2 * h + i) - (a + 2 * b + c)) / (8 * dy)
-                loc_string[ax_y, ax_x] = 'middle grid'
-
-    hpot = np.hypot(abs(dzdy), abs(dzdx))
-    slopes_angle = np.degrees(np.arctan(hpot))
-    if kwargs['plot']:
-        slopes_angle[(slopes_angle < min) | (slopes_angle > max)]
-
-        plt.figure(figsize=figsize)
-        plt.pcolormesh(xs, ys, slopes_angle, cmap=plt.cm.gist_yarg, vmax=max, vmin=min)
-
-        plt.colorbar()
-        plt.tight_layout()
-        plt.show()
-
-    return slopes_angle
+def cal_slope(nums):
+    m = len(nums)
+    n = len(nums[0])
+    slope = np.zeros((m, n), dtype=float)
+    for i in range(m):
+        for j in range(n):
+            slope[i][j] = cal_single_point_slope(nums, m, n, i, j, 5, 5)
+    return slope
 
 
 if __name__ == '__main__':
-    file = open("../static/JiuHuaMountain.XYZ", 'r')
-    content = file.read()
-    # print(len(content))
-    # XYZ = pd.read_csv('xyz_file')
-    slopes = gradient(content)
+    # nums = np.array([[50, 45, 50],
+    #                  [30, 30, 30],
+    #                  [8, 10, 10]])
+    nums = np.random.random((20, 20))
+    slope = cal_slope(nums)
+    # sns.heatmap(slope, linewidths=0.02, vmax=100, vmin=0, cmap='rainbow')
+
+    im2 = plt.matshow(slope, norm=matplotlib.colors.Normalize(vmin=0, vmax=1, clip=False))
+    cb2 = plt.colorbar(im2)
+    # 更改坐标轴位置和刻度，方便后面划分网格
+    ax = plt.gca()
+    ax.set_xticks(np.arange(-0.5, len(nums[0]), 1))
+    ax.set_yticks(np.arange(-0.5, len(nums), 1))
+    ax.set_xticklabels(np.arange(0, len(nums[0]) + 1, 1))
+    ax.set_yticklabels(np.arange(0, len(nums) + 1, 1))
+    ax.grid(color='black', linestyle='-', linewidth=1)
+
+    plt.show()
